eddf98fb4f776233219629b6f4efe49fb1dbcc17
fanyingfx <fanyingfx@outlook.com>
2024-10-26 10:52:20 +0800

    add export

---
+++ export.py
@@ -0,0 +1,34 @@
+import sqlite3
+import csv
+
+DATABASE_NAME = "sentences.db"
+db = sqlite3.connect(DATABASE_NAME)
+
+
+def emphasize_word(word_attrs):
+    word = word_attrs[0]
+    content: str = word_attrs[1]
+    definition = word_attrs[2]
+    description = word_attrs[3]
+
+    content = content.replace(word, f"<b>{word}</b>")
+    return (word, content, definition, description)
+
+
+def save_file(words):
+    # add # to indicate it's the header
+    # https://docs.ankiweb.net/importing/text-files.html#file-headers
+
+    fieldnames = ["#word", "sentence", "definition", "description"]
+    with open("sentence.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(fieldnames)
+        writer.writerows(word for word in words if word[1])
+
+
+cur = db.execute(
+    "SELECT words.word,sentences.content, definition,sentences.descriptions FROM words  LEFT JOIN sentences  ON words.sentence_id = sentences.id order by words.id desc"
+)
+words = cur.fetchall()
+words = [emphasize_word(word) for word in words]
+save_file(words)
